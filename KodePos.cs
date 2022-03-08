356316f4c235c42214c5779c904588bcbb569155
realizyy <realizy@student.telkomuniversity.ac.id>
2022-03-09 06:17:57 +0800

    menambahkan implementasi dengan table-driven

---
+++ KodePos.cs
@@ -0,0 +1,27 @@
+﻿namespace tpmodul3_1302204035;
+
+public class KodePos
+{
+    private int kodePos;
+    private string kelurahan;
+
+    public int getKodePos()
+    {
+        return kodePos;
+    }
+    
+    public void setKodePos(int kodePos)
+    {
+        this.kodePos = kodePos;
+    }
+    
+    public void setKelurahan(string kelurahan)
+    {
+        this.kelurahan = kelurahan;
+    }
+    
+    public string getKelurahan()
+    {
+        return kelurahan;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using tpmodul3_1302204035;
+
+//membuat data KodePos
+void CreateData()
+{
+    KodePos kd1 = new KodePos();
+    KodePos kd2 = new KodePos();
+    KodePos kd3 = new KodePos();
+    KodePos kd4 = new KodePos();
+    KodePos kd5 = new KodePos();
+    KodePos kd6 = new KodePos();
+    KodePos kd7 = new KodePos();
+    KodePos kd8 = new KodePos();
+    KodePos kd9 = new KodePos();
+    KodePos kd10 = new KodePos();
+    KodePos kd11 = new KodePos();
+    
+    kd1.setKelurahan("Batununggal");
+    kd2.setKelurahan("Kujangsari");
+    kd3.setKelurahan("Mengger");
+    kd4.setKelurahan("Wates");
+    kd5.setKelurahan("Cijaura");
+    kd6.setKelurahan("Jatisari");
+    kd7.setKelurahan("Margasari");
+    kd8.setKelurahan("Sekejati");
+    kd9.setKelurahan("Kebonwaru");
+    kd10.setKelurahan("Maleer");
+    kd11.setKelurahan("Samoja");
+    
+    kd1.setKodePos(40266);
+    kd2.setKodePos(40287);
+    kd3.setKodePos(40267);
+    kd4.setKodePos(40256);
+    kd5.setKodePos(40287);
+    kd6.setKodePos(40286);
+    kd7.setKodePos(40286);
+    kd8.setKodePos(40286);
+    kd9.setKodePos(40272);
+    kd10.setKodePos(40274);
+    kd11.setKodePos(40273);
+    
+    Console.Write("Kelurahan : " + kd1.getKelurahan() + "\n" + "Kode Pos : " + kd1.getKodePos());
+}
+
+CreateData();
